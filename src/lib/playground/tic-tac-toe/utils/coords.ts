14e13a25ffd0408654de6199e87d2ca27f883538
Anthony Brown <ac524.brown@gmail.com>
2022-04-28 06:51:37 -0700

    Fixed incorrect movement for the center tile.

--- src/lib/playground/tic-tac-toe/utils/coords.ts
+++ src/lib/playground/tic-tac-toe/utils/coords.ts
@@ -43,7 +43,7 @@ export const checks : GetCoordChecksLists = [
     ],
     // Checks for Coord 4
     [
-        getAdjacentRight,
+        getAdjacentLeftRight,
         getAdjacentUpDown,
         getDiagonalUpLeftDownRight,
         getDiagonalUpRightDownLeft
