d8bb970bf038ea970b3ecc9682b02b536c858707
Kelsey Mailau <kelseymailau@gmail.com>
2022-04-27 21:35:34 -0700

    add styles to button

--- src/routes/kelsey.svelte
+++ src/routes/kelsey.svelte
@@ -5,7 +5,6 @@
 
 	const handleClick = () => {
         timesClicked++;
-
     }
 </script>
 
@@ -13,4 +12,25 @@
 	<title>Kelsey</title>
 </svelte:head>
 <!-- <h1>Click the button</h1> -->
-<button on:click={handleClick}>{`You've clicked me ${timesClicked} times`}</button>
+<div class="btn-div">
+    <button class="btn" on:click={handleClick}>{`You've clicked me ${timesClicked} times`}</button>
+</div>
+
+<style>
+    .btn-div {
+        width: 100vw;
+        height: 100vh;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .btn {
+        height: 40px;
+        border: none;
+        padding: 8px;
+        background-color: rgb(145, 178, 189);
+        cursor: pointer;
+        border-radius: 5px;
+    }
+</style>
