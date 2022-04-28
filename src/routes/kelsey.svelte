bc728473f2c29f163a0391c7602d33367fcda14f
Kelsey Mailau <kelseymailau@gmail.com>
2022-04-27 21:27:53 -0700

    add button to page

---
+++ src/routes/kelsey.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    import { createEventDispatcher } from 'svelte'
+    const dispatch = createEventDispatcher();
+    let timesClicked = 0;
+
+	const handleClick = () => {
+        timesClicked++;
+
+    }
+</script>
+
+<svelte:head>
+	<title>Kelsey</title>
+</svelte:head>
+<!-- <h1>Click the button</h1> -->
+<button on:click={handleClick}>{`You've clicked me ${timesClicked} times`}</button>
