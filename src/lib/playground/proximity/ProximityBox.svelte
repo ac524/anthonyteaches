53d845014be0ee2b8a1350f1b409929bc5fd75b4
Anthony Brown <ac524.brown@gmail.com>
2022-05-01 19:45:09 -0700

    Various updates and new use cases for the proximity detection.

--- src/lib/playground/proximity/ProximityBox.svelte
+++ src/lib/playground/proximity/ProximityBox.svelte
@@ -1,23 +1,38 @@
 <script lang=ts>
-    import { onMount } from "svelte";
-    import { mouseX, mouseY } from "./stores";
+    import type { InProxDetails } from "./type";
+    import { onMount, createEventDispatcher } from "svelte";
+    import { mouseX, mouseY, scrollY } from "./stores";
 
     export let radius : number = 20;
 
     let box: HTMLDivElement;
+    let bounds: DOMRect;
     let inTopProx : boolean = false;
     let inBottomProx : boolean = false;
     let inLeftProx : boolean = false;
     let inRightProx : boolean = false;
     let inProx : boolean = false;
     let isInWindow = false;
+    let ratio : number;
+
+    const dispatch = createEventDispatcher<{ inprox: InProxDetails, outprox: undefined }>();
+
+    const dispatchInProx = () => dispatch("inprox", {
+        bounds,
+        inTopProx,
+        inBottomProx,
+        inLeftProx,
+        inRightProx,
+        radius,
+        ratio
+    });
 
     onMount(() => {
         let options = {
             rootMargin: '20px',
         };
 
-        // TODO Shared observer from store?
+        // TODO Create/Share a single observer as a dependency?
         let observer = new IntersectionObserver((entries) => {
 
             entries.forEach(entry => isInWindow = entry.isIntersecting);
@@ -27,36 +42,73 @@
         observer.observe( box );
     });
 
-    $ : {
+    const determineProx = ( mouseX: number, mouseY: number ) => {
+        const wasInProx = inProx;
         if( isInWindow ) {
-            const bounds = box.getBoundingClientRect();
+            bounds = box.getBoundingClientRect();
 
-            const withinX = $mouseX >= (bounds.left-radius) && $mouseX <= (bounds.right+20);
+            const isInBox = mouseX > bounds.left && mouseX < bounds.right && mouseY > bounds.top && mouseY < bounds.bottom;
+
+            if( isInBox ) {
+                const inTop = mouseY < (bounds.top + (bounds.height/2));
+                const inLeft = mouseX < (bounds.left + (bounds.width/2));
+                inTopProx = inTop;
+                inBottomProx = !inTop;
+                inLeftProx = inLeft;
+                inRightProx = !inLeft;
+                ratio = 1;
+                return dispatchInProx();
+            }
+
+            const withinX = mouseX >= (bounds.left-radius) && mouseX <= (bounds.right+20);
 
             if( withinX ) {
-                const isInTopProx = $mouseY >= (bounds.top-radius) && $mouseY < bounds.top;
+                const isInTopProx = mouseY >= (bounds.top-radius) && mouseY < bounds.top;
                 if(isInTopProx !== inTopProx) inTopProx = isInTopProx;
                 if( !inTopProx ) {
-                    const isInBottomProx = $mouseY > (bounds.bottom) && $mouseY <= (bounds.bottom + radius);
+                    const isInBottomProx = mouseY > (bounds.bottom) && mouseY <= (bounds.bottom + radius);
                     if(isInBottomProx !== inBottomProx) inBottomProx = isInBottomProx;
                 }
+            } else {
+                if(inTopProx) inTopProx = false;
+                if(inBottomProx) inBottomProx = false;
             }
             
-            const withinY = $mouseY >= (bounds.top-radius) && $mouseY <= (bounds.bottom+20);
+            const withinY = mouseY >= (bounds.top-radius) && mouseY <= (bounds.bottom+20);
 
             if( withinY ) {
-                const isInLeftProx = $mouseX > (bounds.left-radius) && $mouseX < bounds.left;
+                const isInLeftProx = mouseX > (bounds.left-radius) && mouseX < bounds.left;
                 if(isInLeftProx !== inLeftProx) inLeftProx = isInLeftProx;
 
                 if(!inLeftProx) {
-                    const isInRightProx = $mouseX > (bounds.right) && $mouseX <= (bounds.right + radius);
+                    const isInRightProx = mouseX > (bounds.right) && mouseX <= (bounds.right + radius);
                     if(isInRightProx !== inRightProx) inRightProx = isInRightProx;
                 }
+            } else {
+                if(inLeftProx) inLeftProx = false;
+                if(inRightProx) inRightProx = false;
             }
+        }
+
+        const nowInProx = inTopProx || inBottomProx || inLeftProx || inRightProx;
+
+        if( nowInProx !== inProx ) inProx = nowInProx;
+
+        if( inProx ) {
+            if(inTopProx) ratio = 1 - ((bounds.top - mouseY)/radius);
+            else if(inLeftProx) ratio = 1 - ((bounds.left - mouseX)/radius);
+            else if(inBottomProx) ratio = 1 - ((mouseY - bounds.bottom)/radius);
+            else if(inRightProx) ratio = 1 - ((mouseX - bounds.right)/radius);
+            dispatchInProx();
+        } else if( wasInProx ) {
+            ratio = 0;
+            dispatch("outprox");
         }
     }
 
-    $ : inProx = inTopProx || inBottomProx || inLeftProx || inRightProx;
+    $ : determineProx( $mouseX, $mouseY );
+
+    scrollY.subscribe(() => determineProx( $mouseX, $mouseY ));
 </script>
 
 <div bind:this={box} class:is-near-hover={inProx}>
