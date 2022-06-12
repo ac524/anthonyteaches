aa9ed1bf33963cdbe71eae5bbb7429d8de8aa0ab
Anthony Brown <ac524.brown@gmail.com>
2022-06-11 21:03:04 -0700

    testing KV interaction with a simple counter.

--- src/routes/kvtest.ts
+++ src/routes/kvtest.ts
@@ -3,15 +3,15 @@ import type { RequestEvent } from '@sveltejs/kit/types/private';
 export async function get({ params, platform }: RequestEvent) {
 	// `params.id` comes from [id].js
 
-    const result = await platform.env.FILES.put("test", "test");
+    const result = (await platform.env.FILES.get("count")) || 0;
+
+    const count = parseInt(result) + 1;
+
+    await platform.env.FILES.put("count", count);
 
 	return {
 		body: {
-            platform,
+            count
         }
-	};
-
-	return {
-		status: 404
 	};
 }
