fdc8e24a1ba2a0b7d661d635f7d50b57d03a85e1
Anthony Brown <ac524.brown@gmail.com>
2022-08-23 20:01:26 -0700

    Added turborepo for monorepo orchestration.

--- main/src/routes/index.svelte
+++ main/src/routes/index.svelte
@@ -3,7 +3,7 @@
     <meta name="description" content="Learn about Anthony Brown and the students he teaches and explore free code resources to help you through your own bootcamp experience.">
 </svelte:head>
 
-<h1 class="bg-sky-500">Anthony Teaches!</h1>
+<h1 class="bg-sky-500">Anthony Teaches again!</h1>
 
 <p>Much more to come here in the future! For now you can check out some playgrounds we have as we are learning our current environment.</p>
 
